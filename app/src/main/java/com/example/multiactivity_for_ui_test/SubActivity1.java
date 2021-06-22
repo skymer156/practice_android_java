acfeee5ce156dc35f1363caacdf4217653e0392f
skymer156 <46322826+skymer156@users.noreply.github.com>
2021-06-23 01:09:08 +0900

    add checkbox and Toast only if user check both checkbox in sub activity1, user can back to main activity.

--- app/src/main/java/com/example/multiactivity_for_ui_test/SubActivity1.java
+++ app/src/main/java/com/example/multiactivity_for_ui_test/SubActivity1.java
@@ -2,6 +2,9 @@ package com.example.multiactivity_for_ui_test;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SubActivity1 extends AppCompatActivity {
@@ -12,9 +15,44 @@ public class SubActivity1 extends AppCompatActivity {
 
         setContentView(R.layout.layout1);
 
-        Button returnButton = findViewById(R.id.subButton1);
+        CheckBox[] checkboxes = new CheckBox[2];
+        String[] str = {"no checked","checked","not checked"};
+        String toastMessage = "check all checkboxes";
 
-        returnButton.setOnClickListener( v -> finish());
+        Button returnButton = findViewById(R.id.subButton1);
+        checkboxes[0] = findViewById(R.id.checkBox_1);
+        checkboxes[0].setChecked(false);
+        checkboxes[0].setText(str[0]);
+        checkboxes[1] = findViewById(R.id.checkBox_2);
+        checkboxes[1].setChecked(false);
+        checkboxes[1].setText(str[0]);
+
+
+        checkboxes[0].setOnClickListener( v -> {
+            boolean flag = checkboxes[0].isChecked();
+            if (flag){
+                checkboxes[0].setText(str[1]);
+            }else{
+                checkboxes[0].setText(str[2]);
+            }
+        });
+
+        checkboxes[1].setOnClickListener( v -> {
+            boolean flag = checkboxes[1].isChecked();
+            if (flag){
+                checkboxes[1].setText(str[1]);
+            }else{
+                checkboxes[1].setText(str[2]);
+            }
+        });
+
+        returnButton.setOnClickListener( v -> {
+            if(checkboxes[0].isChecked() && checkboxes[1].isChecked()) finish();
+            else{
+            Toast toast = Toast.makeText(this, toastMessage, Toast.LENGTH_LONG);
+            toast.show();
+            }
+        });
 
     }
 
